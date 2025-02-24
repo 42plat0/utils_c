a0aeba019afe804f873a2d663c88a541ce4533cd
penguin-wings <gytisnaujasis@icloud.com>
2025-02-24 22:29:49 +0200

    Add random num gen util

--- utils.c
+++ utils.c
@@ -4,3 +4,16 @@ int max(int a, int b)
 {
 	return (a >= b) ? a : b;
 }
+
+// *************
+// Random number
+void setSeed()
+{
+    srand(time(NULL));
+}
+
+int getRandom(int max, int exc)
+{
+    return rand() % max + exc;
+}
+// *************
--- utils.h
+++ utils.h
@@ -1,1 +1,12 @@
+#include <stdlib.h>                 // For srand/rand
+#include <time.h>                   // For time()
+
 int max(int, int);
+
+// Sets seed for random num
+void setSeed();
+
+// Generates random number 
+// max = Upper bound
+// exc = Until what value to exclude (eg. exc = 1, means rand+1)
+int getRandom(int max, int exc);
